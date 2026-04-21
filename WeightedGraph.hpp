57a8963d13d819ca8b2d00f4c98ba31ba0e3efbf
Evan Harbaugh <eharbaugh0264@floridapoly.edu>
2026-04-21 15:55:52 -0400

    Update WeightedGraph.hpp Comment structure
    Added basic comments to describer the current setup of the WeightedGraph class

--- WeightedGraph.hpp
+++ WeightedGraph.hpp
@@ -18,21 +18,29 @@ template <typename T>
 class WeightedGraph {
 public:
 
+    //  insertVertex will add the origin flights into the 'vertices' vector
     void insertVertex(const T& v);
+    //  [update to include weights to the edge] insertEdge will add'v2'(destination) and the given 'distance' and 'cost' into the 'edges' vector
     void insertEdge(const T& v1, const T& v2);
+    //  (remove / alter) no need to simply print. Special prints will likely be handled by each of the search functions
     void print() const;
 
+    //  ================================================
+    //  Will not need the simple searches, will be replaced with the specialized searches
     void DFS() const;
     void BFS(int start = 0) const;
     int shortestPath(const T& src, const T& dest) const;
+    //  ================================================
+
     bool isConnected() const;
 
 
 private:
+    //  The main vector 'vertices' that contains the list of every origin airport
     std::vector<T> vertices; 
     std::vector<std::vector<int>> edges; 
 
-    
+    //  Given a input vertex, it will return the index. Returns -1 if not found
     int getVertexIndex(const T& vs) const;
 
     bool hasEdge(int i1, int i2) const;
